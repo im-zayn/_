4e3f4be350e2ec29368952da1182e99e9da32f4e
im-zayn <kanyethethird@gmail.com>
2024-12-22 21:54:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,134 +1,133 @@
 document.addEventListener('DOMContentLoaded', () => {
-            const audioFiles = [
-                'audios/Slave - UnoTheActivist.mp3', 
-                'audios/Gatekeep - PlayboiCarti.mp3', 
-                'audios/Balon Dor.mp3', 
-                'audios/Skin - Otuka.mp3', 
-                'audios/I did it first x Escape - Nemzzz.mp3', 
-            ];
+    const audioFiles = [
+        'audios/Slave - UnoTheActivist.mp3', 
+        'audios/Gatekeep - PlayboiCarti.mp3', 
+        'audios/Balon Dor.mp3', 
+        'audios/Skin - Otuka.mp3', 
+        'audios/I did it first x Escape - Nemzzz.mp3', 
+    ];
 
-            let currentAudioIndex = 0;
-            const fadeScreen = document.querySelector('.fade-screen');
-            const mainContent = document.querySelector('.main-content');
-            let hasPlayed = false;
-            let myAudio = null;  // Declare myAudio outside the function to manage the instance.
+    let currentAudioIndex = Math.floor(Math.random() * audioFiles.length); // Start with a random index
+    const fadeScreen = document.querySelector('.fade-screen');
+    const mainContent = document.querySelector('.main-content');
+    let hasPlayed = false;
+    let myAudio = null;  // Declare myAudio outside the function to manage the instance.
 
-            fadeScreen.addEventListener('click', () => {
-                if (hasPlayed) return;
-                hasPlayed = true;
-                fadeScreen.style.opacity = '0';
-                setTimeout(() => {
-                    mainContent.style.opacity = '1';
-                    startTypeWriter();
-                }, 500);
+    fadeScreen.addEventListener('click', () => {
+        if (hasPlayed) return;
+        hasPlayed = true;
+        fadeScreen.style.opacity = '0';
+        setTimeout(() => {
+            mainContent.style.opacity = '1';
+            startTypeWriter();
+        }, 500);
 
-                setTimeout(() => {
-                    fadeScreen.classList.add('hidden');
-                    mainContent.classList.add('visible');
-                    playAudio();  // Start the first audio
-                }, 1000);
-            });
+        setTimeout(() => {
+            fadeScreen.classList.add('hidden');
+            mainContent.classList.add('visible');
+            playAudio();  // Start the first audio
+        }, 1000);
+    });
 
-            function playAudio() {
-                // Check if the audioFiles array is populated
-                if (audioFiles.length === 0) {
-                    console.error("No audio files found!");
-                    return;
-                }
+    function playAudio() {
+        // Check if the audioFiles array is populated
+        if (audioFiles.length === 0) {
+            console.error("No audio files found!");
+            return;
+        }
 
-                // If there is already an audio instance, stop it before starting the new one
-                if (myAudio) {
-                    myAudio.pause();
-                    myAudio.currentTime = 0; // Reset to the beginning
-                }
+        // If there is already an audio instance, stop it before starting the new one
+        if (myAudio) {
+            myAudio.pause();
+            myAudio.currentTime = 0; // Reset to the beginning
+        }
 
-                myAudio = new Audio(audioFiles[currentAudioIndex]);  // Create new audio instance
-                myAudio.volume = 0.0001;
+        myAudio = new Audio(audioFiles[currentAudioIndex]);  // Create new audio instance
+        myAudio.volume = 0.0001;
 
-                // Play the audio
-                myAudio.play();
+        // Play the audio
+        myAudio.play();
 
-                // Display the current song
-                const currentSongElement = document.getElementById('currentSong');
-                currentSongElement.textContent = audioFiles[currentAudioIndex].split('/').pop().replace('.mp3', '');
+        // Display the current song
+        const currentSongElement = document.getElementById('currentSong');
+        currentSongElement.textContent = audioFiles[currentAudioIndex].split('/').pop().replace('.mp3', '');
 
+        // Handle the end of the song and move to the next one
+        myAudio.addEventListener('ended', function () {
+            currentAudioIndex = (currentAudioIndex + 1) % audioFiles.length;  // Move to next audio
+            playAudio();  // Play the next audio in the array
+        }, false);
 
-                // Handle the end of the song and move to the next one
-                myAudio.addEventListener('ended', function () {
-                    currentAudioIndex = (currentAudioIndex + 1) % audioFiles.length;  // Move to next audio
-                    playAudio();  // Play the next audio in the array
-                }, false);
+        // Gradually increase the volume
+        let volume = 0.0001;
+        const targetVolume = 0.10;
+        const volumeStep = 0.002;
+        const intervalTime = 75;
 
-                // Gradually increase the volume
-                let volume = 0.0001;
-                const targetVolume = 0.10;
-                const volumeStep = 0.002;
-                const intervalTime = 75;
+        const volumeIncrease = setInterval(() => {
+            if (volume < targetVolume) {
+                volume += volumeStep;
+                myAudio.volume = volume;
+            } else {
+                clearInterval(volumeIncrease);
+            }
+        }, intervalTime);
+    }
 
-                const volumeIncrease = setInterval(() => {
-                    if (volume < targetVolume) {
-                        volume += volumeStep;
-                        myAudio.volume = volume;
-                    } else {
-                        clearInterval(volumeIncrease);
-                    }
-                }, intervalTime);
+    function startTypeWriter() {
+        var TxtType = function (el, toRotate, period) {
+            this.toRotate = toRotate;
+            this.el = el;
+            this.loopNum = 0;
+            this.period = parseInt(period, 10) || 2000;
+            this.txt = '';
+            this.tick();
+            this.isDeleting = false;
+        };
+
+        TxtType.prototype.tick = function () {
+            var i = this.loopNum % this.toRotate.length;
+            var fullTxt = this.toRotate[i];
+
+            if (this.isDeleting) {
+                this.txt = fullTxt.substring(0, this.txt.length - 1);
+            } else {
+                this.txt = fullTxt.substring(0, this.txt.length + 1);
             }
 
-            function startTypeWriter() {
-                var TxtType = function (el, toRotate, period) {
-                    this.toRotate = toRotate;
-                    this.el = el;
-                    this.loopNum = 0;
-                    this.period = parseInt(period, 10) || 2000;
-                    this.txt = '';
-                    this.tick();
-                    this.isDeleting = false;
-                };
+            this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
-                TxtType.prototype.tick = function () {
-                    var i = this.loopNum % this.toRotate.length;
-                    var fullTxt = this.toRotate[i];
+            var that = this;
+            var delta = 200 - Math.random() * 100;
 
-                    if (this.isDeleting) {
-                        this.txt = fullTxt.substring(0, this.txt.length - 1);
-                    } else {
-                        this.txt = fullTxt.substring(0, this.txt.length + 1);
-                    }
+            if (this.isDeleting) { delta /= 2; }
 
-                    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-
-                    var that = this;
-                    var delta = 200 - Math.random() * 100;
-
-                    if (this.isDeleting) { delta /= 2; }
-
-                    if (!this.isDeleting && this.txt === fullTxt) {
-                        delta = this.period;
-                        this.isDeleting = true;
-                    } else if (this.isDeleting && this.txt === '') {
-                        this.isDeleting = false;
-                        this.loopNum++;
-                        delta = 500;
-                    }
-
-                    setTimeout(function () {
-                        that.tick();
-                    }, delta);
-                };
-
-                var elements = document.getElementsByClassName('typewrite');
-                for (var i = 0; i < elements.length; i++) {
-                    var toRotate = elements[i].getAttribute('data-type');
-                    var period = elements[i].getAttribute('data-period');
-                    if (toRotate) {
-                        new TxtType(elements[i], JSON.parse(toRotate), period);
-                    }
-                }
-
-                var css = document.createElement("style");
-                css.type = "text/css";
-                css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-                document.body.appendChild(css);
+            if (!this.isDeleting && this.txt === fullTxt) {
+                delta = this.period;
+                this.isDeleting = true;
+            } else if (this.isDeleting && this.txt === '') {
+                this.isDeleting = false;
+                this.loopNum++;
+                delta = 500;
             }
-        });
+
+            setTimeout(function () {
+                that.tick();
+            }, delta);
+        };
+
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i = 0; i < elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+                new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    }
+});
